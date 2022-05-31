2d65082f765494253cd3922a8bf22d33db7106ad
primsbug <92596219+primsbug@users.noreply.github.com>
2022-05-31 16:47:47 +0600

    Add files via upload

---
+++ kurs/connect.php
@@ -0,0 +1,12 @@
+
+
+
+
+<?php
+
+$connect = mysqli_connect($hostname= 'localhost', $username = 'root', $password ='', $database ='kurs');
+
+if(!$connect) {
+    die('Error');
+}
+
---
+++ kurs/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <!--        //Ссылка данной страницы-->
+    <title> Курсовая </title>
+    <!--        // Название вкладки-->
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <!--         // Класс logo для блока с названием сайта-->
+            <a class="h" href="index.php">MonitorOmSTU</a>
+            <!--         // Ссылка при нажатии на название с классом h-->
+        </div>
+        <div class="menu">
+            <!--         // Класс menu для блока навигации-->
+            <ul>
+                <li><p></p></li>
+            </ul>
+        </div>
+        <div>
+            <!--         // Блок с ссылками в навигационной панели  -->
+            <a class = "log" href="rating.php">Рейтинг</a>
+            <a class = "log" href="About_us.html">О нас</a>
+            <a class = "log" href="structure.html">Структура работы</a>
+        </div>
+    </header>
+    <fieldset class="card">
+        <!--         // Поле для текста с классом card-->
+        <p class="card-text">MonitorOmSTU - сайт, который мы разработали для того чтобы упростить анализ деятельности ОмГТУ. Данный сайт даёт каждому желающему доступ к информации об учебной деятельности нашего вуза. Мы надеемся, что наш проект помог вам!</p>
+        <!--            // Класс card-text для оформления текста-->
+    </fieldset>
+</body>
+</html>
